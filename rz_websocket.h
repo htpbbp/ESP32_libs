7b7ddde79c6a469096aabb29bc5ac452a4f8067b
Bugs Bunny Productions <37730925+htpbbp@users.noreply.github.com>
2018-10-14 15:17:01 +0200

    Update rz_websocket.h
    README with sample code included

--- rz_websocket.h
+++ rz_websocket.h
@@ -27,3 +27,70 @@ void rz_webserver_start()
 
 #endif 
 
+
+/********************** README *********************
+ 
+ ### ESP32 Main =>
+ 
+ #include "rz_websocket.h"
+ 
+ void setup()
+ {
+    Serial.begin(115200);
+    delay(10);
+    // ... setup your WiFi and so on ...
+
+    rz_webserver_start();
+ }
+ 
+void loop()
+{
+  WiFiClient client = webserver.available();
+  if (client.connected() && webSocketServer.handshake(client)) 
+  {
+    String data;      
+    while (client.connected()) 
+    {
+      data = webSocketServer.getData();
+      if (data.length() > 0) 
+      {
+        Serial.println(data);
+        webSocketServer.sendData(data);
+      }
+      delay(10); // Delay needed for receiving the data correctly
+    }
+    Serial.println("The client disconnected");
+    delay(100);
+  }
+   
+  delay(100);
+}
+
+### ESP32 Main <=
+
+### Websocket Client =>
+
+#!/usr/bin/python
+
+import websocket
+import time
+ 
+ws = websocket.WebSocket()
+ws.connect("ws://192.168.xxx.xxx:80/")
+ 
+i = 0
+nrOfMessages = 200
+ 
+while i<nrOfMessages:
+    ws.send("message nr: " + str(i))
+    result = ws.recv()
+    print(result)
+    i=i+1
+    time.sleep(1)
+ 
+ws.close() 
+
+### Websocket Client <= 
+
+*/
+
